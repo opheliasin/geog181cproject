74de76d0ac0c5be9f560d79e045693bf874e23be
daisyyan22 <63264433+daisyyan22@users.noreply.github.com>
2021-05-20 17:30:31 -0700

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -61,6 +61,76 @@ print "done with using geometry object."
 #Insert cursor to create and display new table showing 10 nearest vaccination
 #sites with most relevant information for user 
 
+# # # Network Analysis # # # In Progress -DY # # #
+
+#Import system modules
+import arcpy
+from arcpy import env
+
+try:
+    #Check out the Network Analyst extension license
+    arcpy.CheckOutExtension("Network")
+
+    #Set environment settings
+    env.workspace = folderpath
+    env.overwriteOutput = True
+    
+    #Set local variables
+    inNetworkDataset = "//Covid-19_Vaccination_Provider_Locations_in_the_United_States_cleaned_Mari.shp"
+    outNALayerName = "Vac_Sites"
+    impedanceAttribute = "Drivetime" #where is this attribute? -DY
+    accumulateAttributeName = ["Meters"] 
+    inFacilities = "//Warehouses" #change -DY
+    inIncidents = "//Stores" #change -DY
+    outLayerFile = "C://" + "/" + outNALayerName + ".lyr"
+    
+    #Create a new closest facility analysis layer. Apart from finding the drive 
+    #time to the closest warehouse, we also want to find the total distance. So
+    #we will accumulate the "Meters" impedance attribute.
+    outNALayer = arcpy.na.MakeClosestFacilityLayer(inNetworkDataset,outNALayerName,
+                                                   impedanceAttribute,"TRAVEL_TO",
+                                                   "",1, accumulateAttributeName,
+                                                   "NO_UTURNS")
+    
+    #Get the layer object from the result object. The closest facility layer can 
+    #now be referenced using the layer object.
+    outNALayer = outNALayer.getOutput(0)
+    
+    #Get the names of all the sublayers within the closest facility layer.
+    subLayerNames = arcpy.na.GetNAClassNames(outNALayer)
+    #Stores the layer names that we will use later
+    facilitiesLayerName = subLayerNames["Facilities"] #change attribute -DY
+    incidentsLayerName = subLayerNames["Incidents"] #change attribute -DY
+    
+    ##Load the warehouses as Facilities using the default field mappings and 
+    ##search tolerance
+    #arcpy.na.AddLocations(outNALayer, facilitiesLayerName, inFacilities, "", "")
+    
+    ##Load the Stores as Incidents. Map the Name property from the NOM field
+    ##using field mappings
+    #fieldMappings = arcpy.na.NAClassFieldMappings(outNALayer, incidentsLayerName)
+    #fieldMappings["Name"].mappedFieldName = "NOM"
+    #arcpy.na.AddLocations(outNALayer, incidentsLayerName, inIncidents,
+                          #fieldMappings,"")
+    
+    #Solve the closest facility layer
+    arcpy.na.Solve(outNALayer)
+    
+    #Save the solved closest facility layer as a layer file on disk with 
+    #relative paths
+    arcpy.management.SaveToLayerFile(outNALayer,outLayerFile,"RELATIVE")
+    
+    print "Script completed successfully"
+
+except Exception as e:
+    # If an error occurred, print line number and error message
+    import traceback, sys
+    tb = sys.exc_info()[2]
+    print "An error occurred on line %i" % tb.tb_lineno
+    print str(e)
+
+ # # # # # # #
+
 folderPath = r"C:\Users\cnmre\OneDrive\Documents\181C GIS Programming and Dev\geog181cproject"
 
 import arcpy, os
