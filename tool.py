befb88383706a8e199e75644abd27d58b74b8c5d
opheliasin <76908733+opheliasin@users.noreply.github.com>
2021-06-05 17:15:38 -0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -29,35 +29,38 @@ pt.X = long #p[0]
 pt.Y = lat #p[1]
 ptGeom.append(arcpy.PointGeometry(pt))
 
-out_name = "origin_point.shp"
+out_name = arcpy.GetParameterAsText(5) 
 geometry_type = "POINT"
 template = ""
 has_m = "DISABLED"
 has_z = "DISABLED"
-spatial_reference = arcpy.SpatialReference("NAD 1983")
+spatial_reference = arcpy.GetParameterAsText(6) 
 
 arcpy.CreateFeatureclass_management(folderpath, out_name, geometry_type, template, has_m, has_z, spatial_reference)
 arcpy.CopyFeatures_management(ptGeoms, out_name)
 
-drive_thro = arcpy.GetParameterAsText(5) 
-appt_only = arcpy.GetParameterAsText(6) 
-call_first = arcpy.GetParameterAsText(7) 
-Wheelchair = arcpy.GetParameterAsText(8) 
+drive_thro =  arcpy.GetParameterAsText(6) 
+appt_only = arcpy.GetParameterAsText(7) 
+call_first = arcpy.GetParameterAsText(8) 
+wheelchair = arcpy.GetParameterAsText(9) 
 
 where_clause = "" #declare where_clause 
 
 if drive_thro: 
   where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + " r\ = ' " + drive_thro + " r\' " 
 if appt_only: 
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'appt_only'"
-if call_first: 
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'call_first'"
-if Wheelchair: 
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'Wheelchair'"
+  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + " r\ = ' " + appt_only + " r\' " 
+  #where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'appt_only'"
+if call_first:
+  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + " r\ = ' " + call_first + " r\' " 
+  #where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'call_first'"
+if wheelchair: 
+  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + " r\ = ' " + wheelchair + " r\' " 
+  #where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "Wheelchair") + "= 'Wheelchair'"
   
 arcpy.SelectLayerByAttribute_management("vac_sites", "SUBSET_SELECTION", where_clause)
 
-vac_sites_selected = "vac_sites_selected.shp"
+vac_sites_selected =  arcpy.GetParameterAsText(10) 
 arcpy.CreateFeatureclass_management(folderpath, vac_sites_selected, geometry_type, template, has_m, has_z, spatial_reference)
 arcpy.CopyFeatures_management("vac_sites", vac_sites_selected)
 
