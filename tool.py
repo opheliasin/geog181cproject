d1dd704cc1848791eed52afbd43a17d5dd7f8373
daisyyan22 <63264433+daisyyan22@users.noreply.github.com>
2021-06-07 07:53:12 -0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -47,6 +47,51 @@ arcpy.CopyFeatures_management(ptGeom, out_geom)
 arcpy.AddMessage("Finish setting up starting point.")
 
 #-----------------------------------------------
+
+# arcpy.CreateFeatureclass_management(folderpath, out_name, geometry_type, template, has_m, has_z, sr)
+# out_geom = os.path.join(folderpath, out_name)
+# arcpy.CopyFeatures_management(ptGeom, out_geom)
+
+drive_thro = arcpy.GetParameterAsText(5)
+appt_only = arcpy.GetParameterAsText(6) 
+call_first = arcpy.GetParameterAsText(7) 
+wheelchair = arcpy.GetParameterAsText(8) 
+
+where_clause = "" #declare where_clause 
+
+if drive_thro == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'Yes'"
+elif drive_thro == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'No'"
+if appt_only == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'Yes'"
+elif appt_only == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'No'"
+if call_first == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'Yes'"
+elif call_first == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'No'"
+if wheelchair == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + "= 'Yes'"
+elif wheelchair == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + "= 'No'"
+
+arcpy.SelectLayerByAttribute_management("vac_sites", "SUBSET_SELECTION", where_clause)
+vac_sites_selected = arcpy.GetParameterAsText(10)
+
+vac_sites_selected_dir = os.path.dirname(vac_sites_selected)
+vac_sites_selected_basename = os.path.basename(vac_sites_selected)
+
+arcpy.CreateFeatureclass_management(vac_sites_selected_dir, vac_sites_selected_basename, geometry_type, template, has_m, has_z, sr)
+
+#out_fc = os.path.join(folderpath, vac_sites_selected)
+arcpy.CopyFeatures_management("vac_sites", vac_sites_selected)
+
+arcpy.Delete_management(scratch_name)
+
+arcpy.AddMessage("{0} has successfully outputted to .".format(vac_sites_selected_basename, vac_sites_selected_dir))
+
+#-----------------------------------------------
 # Network Analysis
 
 #Check out the Network Analyst extension license
@@ -109,52 +154,6 @@ arcpy.conversion.FeatureClassToShapefile(outNAlayer, TEMP)
 
 routes = arcpy.mapping.ListLayers(outNALayer, "Routes")[0]
 arcpy.conversion.FeatureClassToShapefile(routes, TEMP)
-
-#-----------------------------------------------
-
-# arcpy.CreateFeatureclass_management(folderpath, out_name, geometry_type, template, has_m, has_z, sr)
-# out_geom = os.path.join(folderpath, out_name)
-# arcpy.CopyFeatures_management(ptGeom, out_geom)
-
-drive_thro = arcpy.GetParameterAsText(5)
-appt_only = arcpy.GetParameterAsText(6) 
-call_first = arcpy.GetParameterAsText(7) 
-wheelchair = arcpy.GetParameterAsText(8) 
-
-where_clause = "" #declare where_clause 
-
-if drive_thro == 'Yes':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'Yes'"
-elif drive_thro == 'No':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'No'"
-if appt_only == 'Yes':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'Yes'"
-elif appt_only == 'No':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'No'"
-if call_first == 'Yes':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'Yes'"
-elif call_first == 'No':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'No'"
-if wheelchair == 'Yes':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + "= 'Yes'"
-elif wheelchair == 'No':
-    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + "= 'No'"
-
-arcpy.SelectLayerByAttribute_management("vac_sites", "SUBSET_SELECTION", where_clause)
-vac_sites_selected = arcpy.GetParameterAsText(10)
-
-vac_sites_selected_dir = os.path.dirname(vac_sites_selected)
-vac_sites_selected_basename = os.path.basename(vac_sites_selected)
-
-arcpy.CreateFeatureclass_management(vac_sites_selected_dir, vac_sites_selected_basename, geometry_type, template, has_m, has_z, sr)
-
-#out_fc = os.path.join(folderpath, vac_sites_selected)
-arcpy.CopyFeatures_management("vac_sites", vac_sites_selected)
-
-arcpy.Delete_management(scratch_name)
-
-arcpy.AddMessage("{0} has successfully outputted to .".format(vac_sites_selected_basename, vac_sites_selected_dir))
-
 
 
 
