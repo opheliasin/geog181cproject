ab0509ab8413337b1d6bd4033743c98db1ccf6ad
opheliasin <76908733+opheliasin@users.noreply.github.com>
2021-06-06 20:27:49 -0500

    Update tool.py

--- tool.py
+++ tool.py
@@ -4,23 +4,21 @@
 # Mari Bouwman, Chalsea Montellano, Ryuichi Utsu, Ophelia Sin, Daisy Yan
 #
 #-----------------------------------------------
-
-import arcpy, os
-
-folderpath = arcpy.GetParameterAsText(0)
+import os
 
 #set up environment
-arcpy.env.workspace = folderpath
+TEMP = os.getenv("TEMP") # this is a folder Windows promises will exist
+arcpy.env.scratchWorkspace = TEMP
 arcpy.env.overwriteOutput = True
 
-vac_sites = arcpy.GetParameterAsText(1)
+vac_sites = arcpy.GetParameterAsText(0)
 arcpy.MakeFeatureLayer_management(vac_sites, "vac_sites")
-boundary = arcpy.GetParameterAsText(2)
+boundary = arcpy.GetParameterAsText(1)
 arcpy.MakeFeatureLayer_management(boundary, "boundary")
 arcpy.SelectLayerByLocation_management("vac_sites", "intersect", "boundary")
 
-long = arcpy.GetParameterAsText(3) 
-lat = arcpy.GetParameterAsText(4)
+long = arcpy.GetParameterAsText(2) 
+lat = arcpy.GetParameterAsText(3)
 #p =[]
 #p.extend(long, lat)
 pt = arcpy.Point()
@@ -29,40 +27,76 @@ pt.X = long #p[0]
 pt.Y = lat #p[1]
 ptGeom.append(arcpy.PointGeometry(pt))
 
-out_name = arcpy.GetParameterAsText(5) 
+out_name = "starting_point.shp"
 geometry_type = "POINT"
 template = ""
 has_m = "DISABLED"
 has_z = "DISABLED"
-spatial_reference = arcpy.GetParameterAsText(6) 
+sr = arcpy.GetParameterAsText(4)
 
-arcpy.CreateFeatureclass_management(folderpath, out_name, geometry_type, template, has_m, has_z, spatial_reference)
-arcpy.CopyFeatures_management(ptGeoms, out_name)
+scratch_name = arcpy.CreateScratchName("temp",
+                                       "",
+                                       "Shapefile",
+                                       TEMP)
 
-drive_thro =  arcpy.GetParameterAsText(6) 
-appt_only = arcpy.GetParameterAsText(7) 
-call_first = arcpy.GetParameterAsText(8) 
-wheelchair = arcpy.GetParameterAsText(9) 
+#arcpy.CreateFeatureclass_management(TEMP, scratch_name, geometry_type, template, has_m, has_z, sr)
+out_geom = os.path.join(TEMP, scratch_name)
+arcpy.CopyFeatures_management(ptGeom, out_geom)
+
+arcpy.AddMessage("Finish setting up starting point.")
+
+# arcpy.CreateFeatureclass_management(folderpath, out_name, geometry_type, template, has_m, has_z, sr)
+# out_geom = os.path.join(folderpath, out_name)
+# arcpy.CopyFeatures_management(ptGeom, out_geom)
+
+drive_thro = arcpy.GetParameterAsText(5)
+appt_only = arcpy.GetParameterAsText(6) 
+call_first = arcpy.GetParameterAsText(7) 
+wheelchair = arcpy.GetParameterAsText(8) 
 
 where_clause = "" #declare where_clause 
 
-if drive_thro: 
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + " r\ = ' " + drive_thro + " r\' " 
-if appt_only: 
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + " r\ = ' " + appt_only + " r\' " 
-  #where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'appt_only'"
-if call_first:
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + " r\ = ' " + call_first + " r\' " 
-  #where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'call_first'"
-if wheelchair: 
-  where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + " r\ = ' " + wheelchair + " r\' " 
-  #where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "Wheelchair") + "= 'Wheelchair'"
-  
-arcpy.SelectLayerByAttribute_management("vac_sites", "SUBSET_SELECTION", where_clause)
+if drive_thro == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'Yes'"
+elif drive_thro == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "drive_thro") + "= 'No'"
+if appt_only == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'Yes'"
+elif appt_only == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "appt_only") + "= 'No'"
+if call_first == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'Yes'"
+elif call_first == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "call_first") + "= 'No'"
+if wheelchair == 'Yes':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + "= 'Yes'"
+elif wheelchair == 'No':
+    where_clause = where_clause + arcpy.AddFieldDelimiters("vac_sites", "wheelchair") + "= 'No'"
 
-vac_sites_selected =  arcpy.GetParameterAsText(10) 
-arcpy.CreateFeatureclass_management(folderpath, vac_sites_selected, geometry_type, template, has_m, has_z, spatial_reference)
+arcpy.SelectLayerByAttribute_management("vac_sites", "SUBSET_SELECTION", where_clause)
+vac_sites_selected = arcpy.GetParameterAsText(10)
+
+vac_sites_selected_dir = os.path.dirname(vac_sites_selected)
+vac_sites_selected_basename = os.path.basename(vac_sites_selected)
+
+arcpy.CreateFeatureclass_management(vac_sites_selected_dir, vac_sites_selected_basename, geometry_type, template, has_m, has_z, sr)
+
+#out_fc = os.path.join(folderpath, vac_sites_selected)
 arcpy.CopyFeatures_management("vac_sites", vac_sites_selected)
+
+arcpy.Delete_management(scratch_name)
+
+arcpy.AddMessage("{0} has successfully outputted to .".format(vac_sites_selected_basename, vac_sites_selected_dir))
+
+
+
+
+
+
+
+
+
+
 
 
 
